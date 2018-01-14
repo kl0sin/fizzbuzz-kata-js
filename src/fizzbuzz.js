505c72705018f49ecb6662ec65ebc57f4bbf2992
Mateusz Kłosiński <mkklosin@gmail.com>
2018-01-14 15:00:38 +0100

    Fizzbuzz tests and function added

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,2 +1,11 @@
 module.exports = function fizzbuzz(number) {
+  if(number % 15 == 0) {
+    return ('FIZZBUZZ');
+  } else if(number % 5 == 0) {
+    return ('BUZZ');
+  } else if (number % 3 == 0) {
+    return ('FIZZ');
+  } else {
+    return number.toString();
+  }
 };
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -5,4 +5,19 @@ describe('Fizz Buzz', function () {
   it('has tests', function () {
     expect(fizzbuzz).toBeDefined();
   });
+
+  [
+    {number: 1, result: '1'},
+    {number: 2, result: '2'},
+    {number: 3, result: 'FIZZ'},
+    {number: 5, result: 'BUZZ'},
+    {number: 6, result: 'FIZZ'},
+    {number: 9, result: 'FIZZ'},
+    {number: 15, result: 'FIZZBUZZ'},
+    {number: 45, result: 'FIZZBUZZ'}
+  ].forEach(function(testCase) {
+    it('should return ' + testCase.result + ' for ' + testCase.number, function() {
+      expect(fizzbuzz(testCase.number)).toBe(testCase.result);
+    })
+  });
 });
